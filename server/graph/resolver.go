524efac5c984cb67900558f4f5f80966987063d3
Sean aye <hello@seanaye.ca>
2021-03-18 14:34:21 -0400

    update depdency graph

--- server/graph/resolver.go
+++ server/graph/resolver.go
@@ -3,7 +3,7 @@ package graph
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
-import "github.com/seanaye/geog483-final/pkg/user"
+import "github.com/seanaye/geog483-final/server/pkg/user"
 
 type Resolver struct{
 	User user.User
--- server/pkg/user/user_service.go
+++ server/pkg/user/user_service.go
@@ -1,7 +1,5 @@
 package user
 
-import "github.com/seanaye/geog483-final/server/graph/model"
-
 type User interface {
 	CreateSession(name string)
 }
