e160b553ee0a422a6651278655e578548691b91a
Sean aye <hello@seanaye.ca>
2021-03-28 11:05:30 -0400

    finish authentication middleware

--- server/pkg/middleware/auth.go
+++ server/pkg/middleware/auth.go
@@ -1,31 +1,55 @@
 package auth
 
-import(
+import (
+	"context"
 	"net/http"
-	"github.com/seanaye/geog483-final/server/pkg/redis"
+	"strings"
+
 	"github.com/seanaye/geog483-final/server/pkg/jwt"
+	"github.com/seanaye/geog483-final/server/pkg/redis"
 )
+type contextKey struct {
+	name string
+}
+
+var userCtxKey = &contextKey{"user"}
 
 func Middleware(db *redis.RedisService) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			c, err := r.Header.Get("Authorization")
+			c := r.Header.Get("Authorization")
 			
 
 			// allow unauthenticated users
-			if err != nil || c == nil {
+			if c == "" {
 				next.ServeHTTP(w, r)
 				return
 			}
 
-			token, err := jwt.ValidateToken(c)
+			// turn bearer string into token
+			segments := strings.Split(c, " ")
+
+			if segments[0] != "bearer" {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+			}
+
+			token, err := jwt.ValidateToken(segments[1])
 
 			if err != nil {
 				http.Error(w, "Invalid authentication token", http.StatusForbidden)
 				return
 			}
 
+			id := token.Claims["id"]
+			user, err := db.GetUser(id)
+			if err != nil {
+				http.Error(w, "Could not find user in database", http.StatusNotFound)
+			}
 
-		}
+			ctx := context.WithValue(r.Context(), userCtxKey, user)
+
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
 	}
 }
