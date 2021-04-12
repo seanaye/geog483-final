00f0e142079f74e1c6ff164da19d6cff3f613727
Sean aye <hello@seanaye.ca>
2021-04-11 23:19:03 -0400

    use prod

--- client/svelte.config.cjs
+++ client/svelte.config.cjs
@@ -1,5 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
-const static = require('@sveltejs/adapter-static');
+//const static = require('@sveltejs/adapter-static');
+const node = require('@sveltejs/adapter-node')
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,7 +19,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: static(),
+		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
